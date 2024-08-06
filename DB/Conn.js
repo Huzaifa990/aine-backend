28ee38bfc489cd06ce949fb6ca71ed4536246e31
Huzaifa990 <sultanhuzaifa0@gmail.com>
2024-08-06 12:50:56 -0500

    Cluster Added

--- DB/Conn.js
+++ DB/Conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/aine-test").then(()=>{
+mongoose.connect("mongodb+srv://sultanhuzaifa0:12345@huzaifasultan.3wryeup.mongodb.net/aine-test").then(()=>{
     console.log("Connected to DB");
 }).catch((e)=>{
     console.log(e);
